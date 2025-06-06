d7c8e08faa2280a928ee01571bf0987cd70ffb01
Elena Vedeneva <astroshelest@gmail.com>
2025-06-06 17:39:18 +0200

    Created run model

--- app_run/models.py
+++ app_run/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+user = get_user_model()
+
+
+class Run(models.Model):
+    athlete = models.ForeignKey(
+        user,
+        on_delete=models.CASCADE,
+        related_name='runs',
+    )
+    created_at = models.DateTimeField(auto_now=True)
+    comment = models.TextField()
+
+    class Meta:
+        db_table = 'run'
+        verbose_name = 'Забег'
+        verbose_name_plural = 'Забеги'
