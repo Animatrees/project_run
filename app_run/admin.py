ad14a29068601001e6527031309b853cbb164877
Elena Vedeneva <astroshelest@gmail.com>
2025-06-06 17:57:38 +0200

    Created RunModelAdmin

--- app_run/admin.py
+++ app_run/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from app_run.models import Run
+
 # Register your models here.
+@admin.register(Run)
+class RunModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'athlete', 'comment', 'created_at')
+    ordering = ('created_at', )
+    list_filter = ('athlete',)
+    search_fields = ('athlete',)
