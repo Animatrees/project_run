64f869ed5faf70fc2cffc6ad59b936ef5fb0e309
Elena Vedeneva <astroshelest@gmail.com>
2025-06-06 17:39:37 +0200

    Created RunViewSet

--- app_run/views.py
+++ app_run/views.py
@@ -1,12 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import viewsets
 from django.conf import settings
+
+from app_run.models import Run
+from app_run.serializers import RunSerializer
 
 
 @api_view(['GET'])
 def company_details_view(request):
-    return Response ({
+    return Response({
         'company_name': settings.COMPANY_NAME,
         'slogan': settings.SLOGAN,
         'contacts': settings.CONTACTS,
     })
+
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
