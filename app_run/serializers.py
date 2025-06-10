6b9576d49a2c71dddfc306dda15bd629f693047b
Elena Vedeneva <astroshelest@gmail.com>
2025-06-10 11:50:04 +0200

    Added UsersSerializer

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -1,8 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.conf import settings
 from rest_framework import serializers
 from .models import Run
+
+user = get_user_model()
 
 
 class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
         fields = ['id', 'athlete', 'created_at', 'comment']
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = user
+        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name', 'type']
+
+    def get_type(self, obj):
+        role_runner = {value: key for key, value in settings.RUNNER_ROLE.items()}
+        return role_runner[obj.is_staff]
