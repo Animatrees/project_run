eba33e9c2db9b882b0098bf0af546de90de86eac
Elena Vedeneva <astroshelest@gmail.com>
2025-06-10 11:50:17 +0200

    Added GetUsersView

--- app_run/views.py
+++ app_run/views.py
@@ -1,10 +1,13 @@
+from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import viewsets
 from django.conf import settings
 
 from app_run.models import Run
-from app_run.serializers import RunSerializer
+from app_run.serializers import RunSerializer, UsersSerializer
+
+user = get_user_model()
 
 
 @api_view(['GET'])
@@ -19,3 +22,18 @@ def company_details_view(request):
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+
+class GetUsersView(viewsets.ReadOnlyModelViewSet):
+    queryset = user.objects.all()
+    serializer_class = UsersSerializer
+
+    def get_queryset(self):
+        qs = self.queryset.exclude(is_superuser=True)
+        user_type = self.request.query_params.get('type', None)
+        is_staff = settings.RUNNER_ROLE.get(user_type, None)
+
+        if is_staff is not None:
+            qs = qs.filter(is_staff=is_staff)
+
+        return qs
--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-g*g7qv5h^5=xvq@zka9b-@0wo@yp5e2yo6c)2_7wdmcbf(2p$9
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -91,7 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -102,7 +99,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -118,3 +114,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 COMPANY_NAME = 'MadRunners'
 SLOGAN = 'Run for your life!'
 CONTACTS = 'Serbia, Novi Sad'
+
+RUNNER_ROLE = {
+    'coach': True,
+    'athlete': False,
+}
