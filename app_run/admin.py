cd21da4016dc0ec29adf4a40178fceadd6b3f978
Elena Vedeneva <astroshelest@gmail.com>
2025-06-19 11:09:16 +0200

    Added status field

--- app_run/admin.py
+++ app_run/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from app_run.models import Run
 
+
 # Register your models here.
 @admin.register(Run)
 class RunModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'athlete', 'comment', 'created_at')
-    ordering = ('created_at', )
+    list_display = ('id', 'athlete', 'comment', 'created_at', 'status')
+    ordering = ('created_at',)
     list_filter = ('athlete',)
     search_fields = ('athlete',)
+    readonly_fields = ('status',)
