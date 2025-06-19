dcbcb526e5761198294b79f7604cd279e760a9b5
Elena Vedeneva <astroshelest@gmail.com>
2025-06-19 11:09:36 +0200

    Created endpoints - start and stop run

--- app_run/views.py
+++ app_run/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from django.conf import settings
+from rest_framework.views import APIView
 
-from app_run.models import Run
+from app_run.models import Run, Status
 from app_run.serializers import RunSerializer, UsersSerializer
 
 user = get_user_model()
@@ -40,3 +42,35 @@ class GetUsersView(viewsets.ReadOnlyModelViewSet):
             qs = qs.filter(is_staff=is_staff)
 
         return qs
+
+
+class RunStartedView(APIView):
+    def post(self, request, run_id):
+        run = get_object_or_404(Run, id=run_id)
+        if run.status != Status.INIT:
+            return Response(
+                {"detail": "Run cannot be started from the current status."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        run.status = Status.IN_PROGRESS
+        run.save()
+        return Response({
+            'status': Status.IN_PROGRESS.label,
+        })
+
+
+class RunStoppedView(APIView):
+    def post(self, request, run_id):
+        run = get_object_or_404(Run, id=run_id)
+        if run.status != Status.IN_PROGRESS:
+            return Response(
+                {"detail": "Run cannot be finished from the current status."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        run.status = Status.FINISHED
+        run.save()
+        return Response({
+            'status': Status.FINISHED.label,
+        })
--- project_run/urls.py
+++ project_run/urls.py
@@ -1,26 +1,8 @@
-"""
-URL configuration for project_run project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 from rest_framework.routers import DefaultRouter
 
-from app_run.views import company_details_view, RunViewSet, GetUsersView
+from app_run.views import company_details_view, RunViewSet, GetUsersView, RunStartedView, RunStoppedView
 
 router = DefaultRouter()
 router.register('api/runs', RunViewSet)
@@ -29,5 +11,7 @@ router.register('api/users', GetUsersView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/company_details/', company_details_view),
+    path('api/runs/<int:run_id>/start/', RunStartedView.as_view(), name='run-started'),
+    path('api/runs/<int:run_id>/stop/', RunStoppedView.as_view(), name='run-stopped'),
     path('', include(router.urls)),
 ]
