4072b667da4f048c1a084df2b739e4e6df4c191d
Elena Vedeneva <astroshelest@gmail.com>
2025-06-19 11:08:30 +0200

    Added Status field for the Run model

--- app_run/models.py
+++ app_run/models.py
@@ -4,6 +4,12 @@ from django.db import models
 user = get_user_model()
 
 
+class Status(models.TextChoices):
+    INIT = 'INIT', 'init'
+    IN_PROGRESS = 'WIP', 'in_progress'
+    FINISHED = 'DONE', 'finished'
+
+
 class Run(models.Model):
     athlete = models.ForeignKey(
         user,
@@ -12,6 +18,11 @@ class Run(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
+    status = models.CharField(
+        max_length=4,
+        choices=Status,
+        default=Status.INIT,
+    )
 
     class Meta:
         db_table = 'run'
